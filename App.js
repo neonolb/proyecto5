1014157ff1d594e298a5213d026f6116e20a5f92
nolbertoha@gmail.com <nohernandez.dwfs14@bootcampudd.cl>
2024-12-07 21:35:54 -0300

    LIBRERIAS

---
+++ App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+        </BrowserRouter>,
+        document.getElementById('root')
+        );
+    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+;
+
+
